d423ab3351df69bdb138d7d166f684179e71c47e
jaqobss <sarunas.nemeiksis@gmail.com>
2018-11-01 22:29:05 +0200

    new 1

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,58 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import moment from 'moment';
+import { Form, HasError, AlertError } from 'vform'
+import swal from 'sweetalert2'
+window.swal = swal;
+
+const toast = swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000
+})
+
+window.toast = toast;
+
+window.Form = Form;
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
+
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import VueProgressBar from 'vue-progressbar'
+Vue.use(VueProgressBar, {
+    color: 'rgb(143, 255,199)',
+    failedColor: 'red',
+    height: '3px'
+})
+
+let routes = [
+    { path: '/dashboard', component: require('./components/Dashboard.vue') },
+    { path: '/profile', component: require('./components/Profile.vue') },
+    { path: '/users', component: require('./components/Users.vue') }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes, // short for 'routes: routes'
+    linkActiveClass: 'active'
+})
+
+
+Vue.filter('upText',  function(text){
+    return text.charAt(0).toUpperCase() + text.slice(1)
+})
+
+
+Vue.filter('myDate', function(created){
+    return moment(created).format("MMMM Do YYYY");
+})
+ 
+window.Fire = new Vue();
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -18,5 +70,6 @@ window.Vue = require('vue');
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router
 });
